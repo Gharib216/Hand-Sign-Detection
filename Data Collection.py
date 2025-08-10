845e6a4325a601edd801434178e49b53b3659917
Gharib216 <medogharib70@gmail.com>
2025-08-10 22:35:15 +0300

    Update Data Capturing.py

--- Data Collection.py
+++ Data Collection.py
@@ -2,15 +2,75 @@ import cv2 as cv
 import HandTrackingModule as htm
 import numpy as np
 import time
+import os
 
+# ===== Configuration =====
+OFFSET = 15
+BACKGROUND_SIZE = (300, 300)
+DATA_FOLDER = 'Data'
+
+MAX_IMAGES = 2000           # Max images for ASL letters/digits
+MAX_NEGATIVE_IMAGES = 3000  # Max images for "NEGATIVE" class
+
+CATEGORIES = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y', 'Z', 'NEGATIVE'
+]
+
+counter_dict = {}
+
+# Count existing images per category (only once at start)
+for cat in CATEGORIES:
+    path = os.path.join(DATA_FOLDER, cat)
+    os.makedirs(path, exist_ok=True)
+    existing_files = [file for file in os.listdir(path) if file.lower().endswith(('.jpg', '.png'))]
+    counter_dict[cat] = len(existing_files) + 1
+
+KEY_CODES = {
+    "UP": 2490368,
+    "DOWN": 2621440,
+    "LEFT": 2424832,
+    "RIGHT": 2555904,
+    "ESC": 27
+}
+
+def caputureHandImg(img, bbox, offset, bg_size):
+    """Crop hand from image and fit into background size."""
+    x1, y1, x2, y2 = bbox
+    cropped = img[y1 - offset:y2 + offset, x1 - offset:x2 + offset]
+    h, w = cropped.shape[:2]
+
+    if h == 0 or w == 0:
+        return None
+
+    aspect_ratio = w / h
+    if aspect_ratio > 1:
+        new_w = bg_size[1]
+        new_h = int(bg_size[1] / aspect_ratio)
+    else:
+        new_h = bg_size[0]
+        new_w = int(bg_size[0] * aspect_ratio)
+
+    new_w = min(new_w, bg_size[1])
+    new_h = min(new_h, bg_size[0])
+
+    resized = cv.resize(cropped, (new_w, new_h))
+    background = np.full((bg_size[0], bg_size[1], 3), 255, dtype='uint8')
+
+    x_offset = (bg_size[1] - new_w) // 2
+    y_offset = (bg_size[0] - new_h) // 2
+    background[y_offset:y_offset + new_h, x_offset:x_offset + new_w] = resized
+
+    return background
+
+# ===== Init =====
 cap = cv.VideoCapture(0)
 detector = htm.HandDetector(max_num_hands=1, min_tracking_confidence=0.7)
 previous_time = 0
-
-# Constants
-OFFSET = 15
-BACKGROUND_SIZE = (300, 300)
-folder = 'Data/C'
+pointer = 10  # start at 'A'
+folder = f"Data/{CATEGORIES[pointer]}"
 counter = 1
 
 
@@ -25,53 +85,41 @@ else:
         handsInfo, frame = detector.findHands(frame)
 
         if handsInfo:
-            background = np.full((BACKGROUND_SIZE[0], BACKGROUND_SIZE[1], 3), 255, dtype='uint8')
             hand = handsInfo[0]
-            x1, y1, x2, y2 = hand["bbox"]
-            imgCrop = frame[y1 - OFFSET : y2 + OFFSET, x1 - OFFSET : x2 + OFFSET]
-
-            height, width = imgCrop.shape[:2]
+            hand_img = caputureHandImg(frame, hand['bbox'], OFFSET, BACKGROUND_SIZE)
             
-            if height == 0 or width == 0:
-                continue
-             
-            aspect_ratio = width / height
-
-            if aspect_ratio > 1:
-                new_width = BACKGROUND_SIZE[1]
-                new_height = int(BACKGROUND_SIZE[1] / aspect_ratio)
-            else:
-                new_height = BACKGROUND_SIZE[0]
-                new_width = int(BACKGROUND_SIZE[0] * aspect_ratio)
-
-
-            new_width = min(new_width, BACKGROUND_SIZE[1])
-            new_height = min(new_height, BACKGROUND_SIZE[0])
-
-            imgResize = cv.resize(imgCrop, (new_width, new_height))
-
-            x_offset = (BACKGROUND_SIZE[1] - new_width) // 2
-            y_offset = (BACKGROUND_SIZE[0] - new_height) // 2
-
-            background[y_offset:y_offset + new_height, x_offset:x_offset + new_width] = imgResize
-   
-            cv.imshow('Hand Img', background)
+            if hand_img is not None:
+                cv.imshow('Hand Img', hand_img)
 
             key = cv.waitKey(1)
 
             if key == ord('s'):
-                cv.imwrite(f'{folder}/Image_{counter}.jpg', background)
-                counter += 1
+                count = counter_dict[CATEGORIES[pointer]]
+                cv.imwrite(f'{folder}/Image_{count}.jpg', hand_img)
+                counter_dict[CATEGORIES[pointer]] += 1  # Update this folder's counter
 
+        key = cv.waitKeyEx(1)
+
+        if key == KEY_CODES['UP']:  # UP
+            pointer = (pointer - 10) % len(CATEGORIES)
+        elif key == KEY_CODES['DOWN']:  # DOWN
+            pointer = (pointer + 10) % len(CATEGORIES)
+        elif key == KEY_CODES['LEFT']:  # LEFT
+            pointer = (pointer - 1) % len(CATEGORIES)
+        elif key == KEY_CODES['RIGHT']:  # RIGHT
+            pointer = (pointer + 1) % len(CATEGORIES)
+        
+        folder = f"Data/{CATEGORIES[pointer]}"
 
         current_time = time.time()
         fps = 1 / (current_time - previous_time)
         previous_time = current_time
         cv.putText(frame, f'FPS: {int(fps)}', (10, 50), cv.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
+        cv.putText(frame, f'Folder: {folder}', (10, 90), cv.FONT_HERSHEY_PLAIN, 2, (255, 255, 0), 2)
 
         cv.imshow('Live Cam', frame)
 
-        if cv.waitKey(1) == 27:
+        if key == KEY_CODES['ESC']:
             break
 
 cap.release()
